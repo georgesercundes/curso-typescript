3afdcff875f8e5496306d328d92656aff6199b66
george.siqueira <gss@ecomp.poli.br>
2022-03-18 08:20:18 -0300

    first commit

---
+++ classes/classes.ts
@@ -0,0 +1,159 @@
+class Data {
+    // Público por padrão
+    dia: number
+    public mes: number
+    ano: number
+
+    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+        this.dia = dia
+        this.mes = mes
+        this.ano = ano
+    }
+}
+
+const aniversario = new Data(3, 11, 1991)
+aniversario.dia = 4
+console.log(aniversario.dia)
+console.log(aniversario)
+
+const casamento = new Data //posso omitir os "()"
+casamento.ano = 2017
+console.log(casamento)
+
+class DataEsperta {
+    constructor(public dia: number = 1, public mes: number = 1,
+        public ano: number = 1970) {
+
+    }
+}
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991)
+aniversario.dia = 4
+console.log(aniversario.dia)
+console.log(aniversario)
+
+const casamentoEsperto = new Data //posso omitir os "()"
+casamento.ano = 2017
+console.log(casamento)
+
+/* 
+Desafio classe Produto
+Atritutos: nome preco e desconto
+Criar o construtor
+Obs 1. Desconto é opcional(valor padrão 0)
+Obs 2. Criar dois produtos: passando dois e três parâmetros
+*/
+
+class Produto {
+    constructor(public nome: string, public preco: number,
+        public desconto: number = 0) {
+    }
+
+    // Também é public
+    precoComDesconto(): number {
+        return this.preco * (1 - this.desconto)
+    }
+
+    public resumo(): string {
+        return `${this.nome} custa R$ ${this.precoComDesconto().toFixed(2)} (${this.desconto * 100}% off)`
+    }
+}
+
+const prod1 = new Produto('Caneta Bic Preta', 4.20)
+prod1.desconto = 0.06
+console.log(prod1.resumo())
+
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15)
+console.log(prod2.resumo())
+
+class Carro {
+    private velocidadeAtual: number = 0
+
+    constructor(public marca: string, public modelo: string,
+        private velocidadeMaxima: number = 200) {
+
+    }
+
+    protected alterarVelocidade(delta: number): number {
+        const novaVelocidade = this.velocidadeAtual + delta
+        const velocidadeValida = novaVelocidade >= 0
+            && novaVelocidade <= this.velocidadeMaxima
+
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade
+        } else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+
+        return this.velocidadeAtual
+    }
+
+    public acelerar(): number {
+        return this.alterarVelocidade(5)
+    }
+
+    public frear(): number {
+        return this.alterarVelocidade(-5)
+    }
+}
+
+const carro1 = new Carro('Ford', 'Ka', 185)
+Array(50).fill(0).forEach(() => carro1.acelerar())
+console.log(carro1.acelerar())
+
+Array(40).fill(0).forEach(() => carro1.frear())
+console.log(carro1.frear())
+
+// Simular "erros"
+// carro1.velocidadeAtual = 300
+// console.log('atual -> ' + carro1.velocidadeAtual)
+
+// carro1.velocidadeMaxima = 500
+// console.log('máxima -> ' + carro1.velocidadeMaxima)
+
+// carro1.alterarVelocidade(150)
+// console.log('atual -> ' + carro1.velocidadeAtual)
+
+class Ferrari extends Carro {
+    constructor(modelo: string, velocidadeMaxima: number) {
+        super('Ferrari', modelo, velocidadeMaxima)
+    }
+
+    public acelerar(): number {
+        return this.alterarVelocidade(20)
+    }
+
+    public frear(): number {
+        return this.alterarVelocidade(-15)
+    }
+}
+
+const f40 = new Ferrari('F40', 324)
+console.log(`${f40.marca} ${f40.modelo}`)
+console.log(f40.acelerar())
+console.log(f40.acelerar())
+console.log(f40.acelerar())
+console.log(f40.frear())
+console.log(f40.frear())
+
+// Getter & Setters
+class Pessoa {
+    private _idade: number = 0
+
+    get idade(): number {
+        return this._idade
+    }
+
+    set idade(valor: number) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor
+        }
+    }
+}
+
+const pessoa1 = new Pessoa
+pessoa1.idade = 10
+console.log(pessoa1.idade)
+
+pessoa1.idade = -3
+console.log(pessoa1.idade)
---
+++ tipos/tipos.ts
@@ -0,0 +1,225 @@
+// String
+let nome: string = 'João'
+console.log(nome)
+// nome = 28
+
+// Numbers
+let idade: number = 27
+// idade = 'Ana'
+idade = 27.5
+console.log(idade)
+
+// Boolean
+let possuiHobbies: boolean = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
+
+// Tipos explícitos
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// minhaIdade = 'idade é 27'
+
+// Array
+let hobbies: any[] = ['Cozinhar', 'Praticar Esportes']
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [100, 200, 300]
+// hobbies = 100
+console.log(hobbies)
+
+// Tuplas
+let endereco: [string, number, string] = ["Av Principal", 99, ""]
+console.log(endereco)
+
+endereco = ["Rua importante", 1260, "Bloco C"]
+console.log(endereco)
+
+// Enums
+enum Cor {
+    Cinza, // 0
+    Verde = 100, // 100
+    Azul = 10, // 2
+    Laranja,
+    Amarelo,
+    Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
+
+console.log(Cor.Azul)
+console.log(Cor.Laranja, Cor.Amarelo)
+console.log(Cor.Verde, Cor.Vermelho)
+
+// Any
+let carro: any = "BMW"
+console.log(carro)
+carro = { marca: 'BMW', ano: 2009 }
+console.log(carro)
+
+// Funções
+function retornaMeuNome(): string {
+    // return minhaIdade
+    return nome
+}
+
+console.log(retornaMeuNome())
+
+function digaOi(): void {
+    console.log("Oi")
+}
+
+digaOi()
+
+function multiplicar(numA: number, numB: number): number {
+    return numA * numB
+}
+
+// console.log(multiplicar(2, "Bia"))
+console.log(multiplicar(4.7, 9))
+
+// Tipo Função
+let calculo: (numeroA: number, numeroB: number) => number
+// calculo = digaOi
+// calculo()
+
+calculo = multiplicar
+console.log(calculo(5, 6))
+
+// Objetos
+let usuario: { nome: string, idade: number } = {
+    nome: 'João',
+    idade: 27
+}
+
+console.log(usuario)
+// usuario = {}
+// usuario = {
+//     name: 'Maria',
+//     age: 31
+// }
+
+usuario = {
+    idade: 31,
+    nome: 'Maria'
+}
+console.log(usuario)
+
+// Desafio
+let funcionario: Funcionario = {
+    supervisores: ["Ana", "Fernando"],
+    baterPonto(horario: number): string {
+        return horario <= 8 ? "Ponto normal" : "Fora do horário"
+    }
+}
+
+console.log(funcionario.supervisores)
+console.log(funcionario.baterPonto(8))
+console.log(funcionario.baterPonto(9))
+
+// Alias
+type Funcionario = {
+    supervisores: string[],
+    baterPonto: (horas: number) => string
+}
+
+let funcionario2: Funcionario = {
+    supervisores: ["Bia", "Marcos"],
+    baterPonto(horario: number): string {
+        return horario <= 8 ? "Ponto normal" : "Fora do horário"
+    }
+}
+
+// Union Types
+let nota: number | string = 10
+console.log(`Minha nota é ${nota}!`)
+nota = '10'
+console.log(`Minha nota é ${nota}!`)
+
+// Checando tipos
+let valor = 30
+
+if (typeof valor === "number") {
+    console.log("É um number!")
+} else {
+    console.log(typeof valor)
+}
+
+// Never
+function falha(msg: string): never {
+    throw new Error(msg)
+}
+
+const produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto() {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha("Precisa ter um nome")
+        }
+        if (this.preco <= 0) {
+            falha('Preco inválido')
+        }
+    }
+}
+
+produto.validarProduto()
+
+// Null
+let altura = 12
+// altura = null 
+let alturaOpcional: null | number = 12
+alturaOpcional = null
+
+type Contato = {
+    nome: string,
+    tel1: string,
+    tel2: string | null
+}
+
+const contato1: Contato = {
+    nome: 'Fulano',
+    tel1: '98765432',
+    tel2: null
+}
+
+console.log(contato1.nome)
+console.log(contato1.tel1)
+console.log(contato1.tel2)
+
+let podeSerNulo = null // any
+podeSerNulo = 12
+console.log(podeSerNulo)
+podeSerNulo = 'abc'
+console.log(podeSerNulo)
+
+// Desafio
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+
+
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number): void {
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
